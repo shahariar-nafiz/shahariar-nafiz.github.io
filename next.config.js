4bc768a93dd25b17800b1817a199e51e2a37cbba
Md. Shahariar Nafiz Irfan <irfanium.bd@gmail.com>
2025-11-26 11:01:21 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
